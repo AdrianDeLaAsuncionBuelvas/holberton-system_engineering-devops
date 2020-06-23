3ac07829dc9e46c72ea34b3acddd9eab2ba55e92
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-06-22 23:52:26 -0500

    add 102 Task Zombie

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+ * infinite_while - infinite Loop
+ * Return: 0 Success or 1 Error
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - function main
+ * Return: 0 Success or 1 Error
+ *
+ */
+
+int main(void)
+{
+	int p = 5;
+	pid_t child_pid;
+
+	while (--p)
+	{
+		child_pid = fork();
+		if (child_pid > 0)
+			printf("Zombie process created, PID: %i\n", child_pid);
+
+		else
+			exit(0);
+	}
+	infinite_while();
+
+	return (0);
+}
