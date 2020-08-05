a2be19adadd26e77e777b0781f4329f51faa0e1e
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-04 21:57:08 -0500

    add 4 Task client configuration file (Puppet)

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
+# connect to a server without typing a password
+
+file_line { 'set off passwd auth':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
-  match  => 'PasswordAuthentication yes',
 }
 
 file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line	 => 'IdentityFile ~/.ssh/holberton',
+  line   => 'IdentityFile ~/.ssh/holberton',
 }
