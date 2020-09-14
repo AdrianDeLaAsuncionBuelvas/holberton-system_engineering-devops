5aa10bfe30070a74e0b5738ec0b64430076b0c5f
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-09-14 18:06:13 -0500

    Add Task 0 Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""REST API returns information about his/her TODO list progress"""
+
+import requests
+import json
+from sys import argv
+
+
+if __name__ == '__main__':
+    _id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(_id)
+
+    response = requests.get(url)
+    jreq = response.json()
+    name = jreq['name']
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    response = requests.get(url)
+    jreq = response.json()
+    tasks = []
+    complete_tasks = []
+    for i in jreq:
+        if i['userId'] == int(_id):
+            tasks.append(i)
+    total_tasks = len(tasks)
+    for i in tasks:
+        if i['completed'] is True:
+            complete_tasks.append(i)
+    print("Employee {} is done with tasks({}/{}):".format(name,
+                                                          len(complete_tasks),
+                                                          total_tasks))
+    for i in complete_tasks:
+        print("\t {}".format(i['title']))
