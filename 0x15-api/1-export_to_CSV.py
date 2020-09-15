ad16cae328712ab9c2052f8a4a354de986b185aa
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-09-14 21:06:29 -0500

    Add Task 1 Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,27 +8,17 @@ from sys import argv
 
 if __name__ == '__main__':
     _id = argv[1]
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(_id)
+    users = 'https://jsonplaceholder.typicode.com/users/{}'.format(_id)
+    todo = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(_id)
 
-    response = requests.get(url)
-    jreq = response.json()
-    name = jreq['name']
-    url = 'https://jsonplaceholder.typicode.com/todos'
-    response = requests.get(url)
-    jreq = response.json()
-    tasks = []
-    completed_tasks = []
-    for task in jreq:
-        if task['userId'] == int(_id):
-            tasks.append(task)
-    total_tasks = len(tasks)
-    for task in tasks:
-            completed_tasks.append({'name': name, 'id': _id,
-                                    'completed': task['completed'],
-                                    'title': task['title']})
+    req_user = requests.get(users).json()
+    req_todo = requests.get(todo).json()
+
     with open('{}.csv'.format(_id), mode='w') as csv_file:
-        fieldnames = ['id', 'name', 'completed', 'title']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames, quotechar='"',
+        writer = csv.writer(csv_file, delimiter=',',
                                 quoting=csv.QUOTE_ALL)
-        for task in completed_tasks:
-            writer.writerow(task)
+        for task in req_todo:
+            writer.writerow([task['userId'],
+                            req_user['name'],
+                            task['completed'],
+                             task['title']])
